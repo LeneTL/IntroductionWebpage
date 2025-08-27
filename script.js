5d0cff917404c613e20c946618e1fe5261c00464
Lene <Lene-tl@hotmail.com>
2025-08-27 11:48:08 +0200

    cardColourAndNewIcon

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+function changeAllCards(color){ //Takk Martin
+    const elements = document.getElementsByClassName("card");
+    for(let i = 0; i < elements.length; i++){
+        elements[i].style.backgroundColor = color
+    }
+}
+
+//get elementById/ClassName funket ikke for card fordi card hadde flere.. ting å se på?(card.card-a card.card-b osv.) HTMLCollection
 function changeLight(){
     document.body.classList.toggle('lightmode')
     if(document.body.classList.contains('lightmode')){
-        (document.getElementById('lightImgToggle').src = 'Image/moon.png') 
-        (document.getElementById('wrapColourChange').style.backgroundColor = '#85b889ff')
-        // (document.getElementById('cardColourChange').style.backgroundColor = '#b7e4bdff')
-        // (document.getElementsByClassName("card").style.backgroundColor = '#345539ff')
-    }else
-        (document.getElementById('lightImgToggle').src = 'Image/sun.png')
-        (document.getElementById('wrapColourChange').style.backgroundColor = '#3b1111')
-        // (document.getElementsByClassName("card").backgroundColor = '#745d2eff')
-
+        document.getElementById('lightImgToggle').src = 'Image/moon.png'
+        document.getElementById('wrapColourChange').style.backgroundColor = '#85b889ff'
+        changeAllCards('#9ec1e8ff')
+        
+    }else{
+        document.getElementById('lightImgToggle').src = 'Image/sun.png'
+        document.getElementById('wrapColourChange').style.backgroundColor = '#3b1111'
+        changeAllCards('#5a5050')
+}
 }
 
-// SLIDESHOW!!! (jeg vet ikke helt hva jeg har gjort her...)
+// SLIDESHOW!!! (jeg vet ikke helt hva jeg har gjort her...) https://www.w3schools.com/howto/howto_js_slideshow.asp
 let slideIndex = 1;
 showSlides(slideIndex);
 
