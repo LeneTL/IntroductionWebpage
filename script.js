74cd14b819249e8b97951a7a09b64ecc47955413
Lene <Lene-tl@hotmail.com>
2025-09-02 10:27:20 +0200

    BodinBoost
    Got help from a friend to understand the Slideshow

--- script.js
+++ script.js
@@ -21,21 +21,29 @@ function changeLight(){
 }
 
 // SLIDESHOW!!! (jeg vet ikke helt hva jeg har gjort her...) https://www.w3schools.com/howto/howto_js_slideshow.asp
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideNumber = 1; //declare to start on slide 1
+showSlides(); //show it (hide all the others)
 
-function numberSlide(n){
-    showSlides(slideIndex += n);
+function numberSlide(number){
+    slideNumber= slideNumber + number; //
+    showSlides();
+    // i++ 
+    // i=i+number
+    // i+=number
 }
 
-function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "block";
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides"); //we pick up the list(Array) (div elements 0-5) from the class
+    if (slideNumber > slides.length) { // if we try to move past the number of images(divs) that we have
+        slideNumber = 1; //then it returns to 1 (because we don't want it to go over into nothing (7)
+    }
+
+    if (slideNumber < 1) { //if we try to move lower than the number of images(divs) we have
+        slideNumber = slides.length; //then we return to the end of the images(divs) we have
+    }
+    for (let i = 0; i < slides.length; i++) { // (i is a counter) the loop goes through all the images(divs) 
+        slides[i].style.display = "none"; // and hides them
+    }
+    slides[slideNumber-1].style.display = "block"; //shows the chosen image(div)
 
 }
